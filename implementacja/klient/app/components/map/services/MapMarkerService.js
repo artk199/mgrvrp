6d4527a83a5805d3385d305607c08cc86a81fa42
Artur K <artex199@gmail.com>
2017-08-19 12:10:01 +0200

    - Zmiana ikony magazynu

--- implementacja/klient/app/components/map/services/MapMarkerService.js
+++ implementacja/klient/app/components/map/services/MapMarkerService.js
@@ -1,23 +1,37 @@
 module.exports = function() {
 
   let paths = [];
+
+  var DepotIcon = new L.Icon.Default({
+    imagePath: 'assets/leaflet/images/',
+    iconUrl: 'marker-icon-2.png',
+  });
+
   /**
   Dodaje marker na mape
   **/
-  this.addMarker = function(point,map,prefix) {
-    let marker = L.marker([point.coordinates.x, point.coordinates.y],{'draggable':true}).addTo(map);
-    marker.bindTooltip(prefix+" #"+point.id).openTooltip();
-    marker.on('dragend', function(event){
+  this.addMarker = function(point, map, prefix) {
+    let opts = {
+      'draggable': true
+    }
+    if (prefix == "Depot") {
+      opts.icon = DepotIcon;
+    }
+    let marker = L.marker(
+      [point.coordinates.x, point.coordinates.y], opts).addTo(map);
+    marker.bindTooltip(prefix + " #" + point.id).openTooltip();
+    marker.on('dragend', function(event) {
       point.coordinates.x = event.target._latlng.lat;
       point.coordinates.y = event.target._latlng.lng;
     });
+    console.log(marker);
     return marker
   }
 
   /**
   Rysuje linie na mapie
   **/
-  this.drawPath = function(srcLat,srcLng,dstLat,dstLng,map,color) {
+  this.drawPath = function(srcLat, srcLng, dstLat, dstLng, map, color) {
     var pointA = new L.LatLng(srcLat, srcLng);
     var pointB = new L.LatLng(dstLat, dstLng);
     var pointList = [pointA, pointB];
@@ -33,8 +47,8 @@ module.exports = function() {
     return path;
   }
 
-  this.clearPaths = function(){
-    angular.forEach(paths,function(path){
+  this.clearPaths = function() {
+    angular.forEach(paths, function(path) {
       path.remove();
     })
   }
